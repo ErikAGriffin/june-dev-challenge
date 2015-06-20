a1f4c3ba32356699aadb00007094123e604b6399
Erik Griffin <griffin.erik@gmail.com>
2015-06-21 00:02:24 +0200

    Working array version

--- r.rb
+++ r.rb
@@ -1,6 +1,11 @@
 class TreeParser
 
   # Handle Double (or even triple!) digit rows!!!
+  # While I 'fixed' the hash method to take up to 9999 rows,
+  # the array method can take a hypothetically infinite number of rows
+  # without key collision.
+  # Thus I will use a nested array to store the tree,
+  # and continue to use a hash for the paths to each node.
 
   attr_reader :paths
 
@@ -21,34 +26,46 @@ class TreeParser
   end
 
   def find_left_parent(key)
-    puts "key: #{key}"
     array = key.to_s.chars
     key1 = []
     key2 = []
     array.each_with_index do |number, index|
       index < (array.length/2.0).ceil ? key1 << number : key2 << number
     end
-    puts "row: #{key1.join} col: #{key2.join}"
+    key1 = key1.join.to_i-1
+    key2 = key2.join.to_i-1
+    (key1.to_s+key2.to_s).to_sym
   end
 
-  def least_sum2(file)
+  def find_right_parent(key)
+    array = key.to_s.chars
+    key1 = []
+    key2 = []
+    array.each_with_index do |number,index|
+      index < (array.length/2.0).ceil ? key1 << number : key2 << number
+    end
+    key1 = key1.join.to_i-1
+    (key1.to_s+key2.join).to_sym
+  end
+
+  def least_sum(file)
     @paths = {}
     time = Time.now
     tree = load_triangle(file)
     tree.each_with_index do |row,rindex|
       row.each_with_index do |number,cindex|
-        # Ahh if I just do rindex+cindex for the hash keys,
-        # could I get collisions??
         key = (rindex.to_s+cindex.to_s).to_sym
-        paths[key] = true
-        find_left_parent(key)
+        # This may be cause of some memory leak..
+        path = (find_smallest_parent(key).dup if find_smallest_parent(key)) || []
+        path << number
+        paths[key] = path
       end
     end
     puts Time.now - time
     puts `ps -o rss -p #{$$}`.strip.split.last.to_i * 1024
   end
 
-  def least_sum(file)
+  def least_sum_old(file)
     @paths = {}
     time = Time.now
     tree = load_triangle2(file)
@@ -62,48 +79,22 @@ class TreeParser
     puts `ps -o rss -p #{$$}`.strip.split.last.to_i * 1024
   end
 
-
-
-
-  # Dear god refactor..
-
-  def second_parent_of(node)
-    temp =node.to_s.chars[0..3].join.to_i-1
-    key1 = [0,0,0]
-    key1 << temp
-    key1 = key1.join.chars[-4..-1].join
-    (key1+(node.to_s.chars[-4..-1].join)).to_sym
-  end
-
-  def first_parent_of(node)
-    temp =node.to_s.chars[0..3].join.to_i-1
-    key1 = [0,0,0]
-    key2 = [0,0,0]
-    key1 << temp
-    key1 = key1.join.chars[-4..-1].join
-    temp = node.to_s.chars[-4..-1].join.to_i-1
-    key2 << temp
-    key2 = key2.join.chars[-4..-1].join
-    (key1+key2).to_sym
-  end
-
-  def load_triangle2(file)
-    result = {}
-    File.open(file).each_with_index do |line,rindex|
-      line.split(" ").each_with_index do |number, cindex|
-        key1 = [0,0,0]
-        key2 = [0,0,0]
-        key1 << rindex
-        key2 << cindex
-        key1 = key1.join.chars[-4..-1].join
-        key2 = key2.join.chars[-4..-1].join
-        result.store((key1+key2).to_sym,number.to_i)
-      end
+  def find_smallest_parent(key)
+    left = find_left_parent(key)
+    right = find_right_parent(key)
+    result = nil
+    if paths[left] && paths[right]
+      result = paths[left].inject{|sum,x| sum+x} < paths[right].inject{|sum,x| sum+x} ? paths[left] : paths[right]
+    elsif paths[left]
+      result = paths[left]
+    elsif paths[right]
+      result = paths[right]
     end
     result
   end
 
-  def find_smallest_parent(node)
+
+  def find_smallest_parent_old(node)
     first = first_parent_of(node)
     second = second_parent_of(node)
     ret = nil
