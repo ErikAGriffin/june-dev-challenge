b280ba3a7f713f58191a0c5be6d8304a20d7f9f8
Erik Griffin <griffin.erik@gmail.com>
2015-06-21 14:42:19 +0200

    Cleaned house

--- lib/TreeParser.rb
+++ lib/TreeParser.rb
@@ -1,23 +1,14 @@
 class TreeParser
 
-  # Also subsequent runs without closing pry causes
-  # the process to grow and grow, if my measurement
-  # method is sound. Why?
-
   class << self
 
     def parse_least_sum(file)
-      time = Time.now  # --- Logging
       tree = load_triangle(file)
       tree.each_with_index do |row,rindex|
-        row.each_with_index do |number,cindex|
-          node = find_smallest_parent(rindex,cindex,tree) || 0
-          node+= number
-          row[cindex] = node
+        row.each_with_index do |node,cindex|
+          row[cindex] = (find_smallest_parent(rindex,cindex,tree) || 0) + node
         end
       end
-      puts Time.now - time # --- Logging
-      puts `ps -o rss -p #{$$}`.strip.split.last.to_i * 1024 # --- Logging
       tree.last.inject{|min,node| min > node ? node : min}
     end
 
