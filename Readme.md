98617b6b9c2ced02bd650606a4967f12372d628f
Erik Griffin <griffin.erik@gmail.com>
2015-06-21 15:55:40 +0200

    Cleaned up, rake was not necessary

--- Readme.md
+++ Readme.md
@@ -6,5 +6,5 @@ Please ensure that the extracted 'challenge' folder is located on your desktop.
 Navigate to the folder within your terminal window and enter the following commands:
 
 `bundle install`
-`rake`
+`chmod +x developer_challenge`
 `export PATH=$HOME/Desktop/challenge:$PATH`
--- spec/tree_parser_spec.rb
+++ spec/tree_parser_spec.rb
@@ -14,12 +14,10 @@ describe TreeParser do
   end
 
   # Note this test is merely to make sure my changes do
-  # not affect output. I have no guarantee the value
-  # given is the correct minimum sum.
+  # not affect output. I have no guarantee that the value
+  # 2768 is the correct minimum sum.
   it 'Finds the minimum path sum of a large triangle' do
     expect(TreeParser.parse_least_sum('data/triangle')).to eq 2768
   end
-
-
 
 end
